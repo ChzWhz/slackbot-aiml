91c9bcd12944d3b9f2051ce31cb99110869eebf6
mfurman <m.furman@live.com>
2017-04-04 15:05:04 -0400

    Update print_bot_id.py

--- utilities/print_bot_id.py
+++ utilities/print_bot_id.py
@@ -1,11 +1,11 @@
 import os
 from slackclient import SlackClient
 
-SLACK_BOT_TOKEN = 
+slack_bot_token = raw_input("What is the Slack Bot Token: ")
 
-BOT_NAME = 
+BOT_NAME = raw_input("What is the Bot's Name: ")
 
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = SlackClient(slack_bot_token)
 
 
 if __name__ == "__main__":
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         # retrieve all users so we can find our bot
         users = api_call.get('members')
         for user in users:
-            if 'name' in user and user.get('name') == BOT_NAME:
+            if 'name' in user and user.get('name') == bot_name:
                 print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
     else:
         print("could not find bot user with the name " + BOT_NAME)
